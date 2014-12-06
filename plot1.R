1554eba708261f296c205c31d9cc72f54cba30cb
Ravi Narasimhan <rnarasim@gmail.com>
2014-12-06 13:34:37 -0500

    additional commentary on the null values with EDT to EST change

--- plot1.R
+++ plot1.R
@@ -22,7 +22,10 @@ unzip ("./project-1/data/household_power_consumption.zip",exdir="./project-1/dat
 power_consumption <- read.table("project-1/data/household_power_consumption.txt",sep=";", header=TRUE,na.strings="?")
 power_consumption$timestamp <- strptime(paste(power_consumption$Date, power_consumption$Time,sep=" "), "%d/%m/%Y %H:%M:%S")
 
-# filter for data between 2007-02-01 and 2007-02-02 and also ensure we don't pull up null timestamps
+# filter for data between 2007-02-01 and 2007-02-02 
+# Note: there are 240 observations in the dataset that happen during DST switch so times betwen 2:00 am and 2:59 am are null
+# you can find those dates here http://wwp.greenwichmeantime.com/daylight-saving-time/usa/dst-2007.htm
+# also ensure we don't pull up null timestamps
 power_consumption_subset <-  power_consumption[power_consumption$timestamp >= '2007-02-01 00:00:00' & power_consumption$timestamp <= '2007-02-02 23:59:59' & !is.na(power_consumption$timestamp ),]
 
 
